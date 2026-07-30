8bedfe02e805931bca381ec3018c78273a47d216
Trikam Devasi <trikam.devasi.cg@outlook.com>
2026-07-30 10:21:55 +0530

    Create 2271-RearrangeArrayElementsBySign

---
+++ 2271-RearrangeArrayElementsBySign/2271-RearrangeArrayElementsBySign.cpp
@@ -0,0 +1,21 @@
+// Last updated: 30/07/2026, 10:21:54
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> result(nums.size());
+        int posIndex = 0;
+        int negIndex = 1;
+        
+        for (int num : nums) {
+            if (num > 0) {
+                result[posIndex] = num;
+                posIndex += 2;
+            } else {
+                result[negIndex] = num;
+                negIndex += 2;
+            }
+        }
+        
+        return result;
+    }
+};
