27a1623afe1c0f9ae4d5a9d8ee6cf2c72bc9b8b6
Trikam Devasi <trikam.devasi.cg@outlook.com>
2026-07-30 10:24:47 +0530

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -0,0 +1,18 @@
+// Last updated: 30/07/2026, 10:24:47
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> groups;
+        for (string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end()); 
+            groups[key].push_back(s);
+        } 
+        vector<vector<string>> result;
+        for (auto const& [key, group] : groups) {
+            result.push_back(group);
+        }
+        
+        return result;
+    }
+};
