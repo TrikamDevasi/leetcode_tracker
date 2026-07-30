da989c3682823fc39f0fae2ca9ef432a36c12fd2
Trikam Devasi <trikam.devasi.cg@outlook.com>
2026-07-30 14:18:48 +0530

    File updated at 30/07/2026, 14:18:47

--- 3163-StringCompressionIII/3163-StringCompressionIII.cpp
+++ 3163-StringCompressionIII/3163-StringCompressionIII.cpp
@@ -1,25 +1,25 @@
-// Last updated: 30/07/2026, 14:09:43
+// Last updated: 30/07/2026, 14:18:47
 1class Solution {
 2public:
 3    string compressedString(string word) {
-4        string comp = "";
-5        int i = 0;
-6        int n = word.length();
-7        
-8        while (i < n) {
-9            char c = word[i];
-10            int count = 0;
-11            
-12            // Count matching characters up to at most 9
-13            while (i < n && word[i] == c && count < 9) {
-14                count++;
-15                i++;
-16            }
-17            
-18            // Append format: [count][character]
-19            comp += to_string(count) + c;
-20        }
+4        string res = ""; 
+5        int count = 1; 
+6        
+7        for(int i = 0; i < word.length(); i++){ 
+8            if(i + 1 < word.length() && word[i] == word[i + 1]){ 
+9                count++; 
+10            } else { 
+11                while (count > 9) {
+12                    res += to_string(9);
+13                    res += word[i];
+14                    count -= 9;
+15                }
+16                res += to_string(count); 
+17                res += word[i]; 
+18                count = 1; 
+19            } 
+20        } 
 21        
-22        return comp;
+22        return res;
 23    }
 24};
