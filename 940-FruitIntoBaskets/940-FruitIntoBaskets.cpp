741e5c9e4adb8ea24e251c8650a9a9436122cd1d
Trikam Devasi <trikam.devasi.cg@outlook.com>
2026-07-30 10:22:44 +0530

    Create 940-FruitIntoBaskets

---
+++ 940-FruitIntoBaskets/940-FruitIntoBaskets.cpp
@@ -0,0 +1,24 @@
+// Last updated: 30/07/2026, 10:22:44
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        unordered_map<int, int> count;
+        int left = 0, max_fruits = 0;
+        
+        for (int right = 0; right < fruits.size(); ++right) {
+            count[fruits[right]]++;
+            
+            while (count.size() > 2) {
+                count[fruits[left]]--;
+                if (count[fruits[left]] == 0) {
+                    count.erase(fruits[left]);
+                }
+                left++;
+            }
+            
+            max_fruits = max(max_fruits, right - left + 1);
+        }
+        
+        return max_fruits;
+    }
+};
