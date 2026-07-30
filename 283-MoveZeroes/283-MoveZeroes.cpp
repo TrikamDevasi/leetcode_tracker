c156fd24ae7bc643a57f992070882815d1a37821
Trikam Devasi <trikam.devasi.cg@outlook.com>
2026-07-30 10:23:29 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -0,0 +1,10 @@
+// Last updated: 30/07/2026, 10:23:29
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0;
+        for(int x:nums)
+        if (x) nums[i++] =x;
+        while(i<nums.size()) nums[i++] = 0;
+    }
+};
