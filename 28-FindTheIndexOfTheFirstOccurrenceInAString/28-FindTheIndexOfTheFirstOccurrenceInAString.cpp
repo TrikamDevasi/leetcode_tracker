94fcada331281d4882b74ea08cfac9477bc9871e
Trikam Devasi <trikam.devasi.cg@outlook.com>
2026-07-30 10:24:59 +0530

    Create 28-FindTheIndexOfTheFirstOccurrenceInAString

---
+++ 28-FindTheIndexOfTheFirstOccurrenceInAString/28-FindTheIndexOfTheFirstOccurrenceInAString.cpp
@@ -0,0 +1,8 @@
+// Last updated: 30/07/2026, 10:24:58
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int index=haystack.find(needle);
+        return index;
+    }
+};
