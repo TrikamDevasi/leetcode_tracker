7fe36248a615ed49099356a21afb811d7309d613
Trikam Devasi <trikam.devasi.cg@outlook.com>
2026-07-30 10:23:40 +0530

    Create 215-KthLargestElementInAnArray

---
+++ 215-KthLargestElementInAnArray/215-KthLargestElementInAnArray.cpp
@@ -0,0 +1,16 @@
+// Last updated: 30/07/2026, 10:23:39
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> min_heap;
+        
+        for (int num : nums) {
+            min_heap.push(num);
+            if (min_heap.size() > k) {
+                min_heap.pop();
+            }
+        }
+        
+        return min_heap.top();
+    }
+};
