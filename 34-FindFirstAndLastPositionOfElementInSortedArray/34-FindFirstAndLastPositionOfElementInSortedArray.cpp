6af7fef3423201d022292d19a2ed23e98b79ba27
Trikam Devasi <trikam.devasi.cg@outlook.com>
2026-07-30 10:24:55 +0530

    Create 34-FindFirstAndLastPositionOfElementInSortedArray

---
+++ 34-FindFirstAndLastPositionOfElementInSortedArray/34-FindFirstAndLastPositionOfElementInSortedArray.cpp
@@ -0,0 +1,32 @@
+// Last updated: 30/07/2026, 10:24:54
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return {findBound(nums, target, true), findBound(nums, target, false)};
+    }
+
+private:
+    int findBound(vector<int>& nums, int target, bool isFirst) {
+        int left = 0, right = nums.size() - 1;
+        int bound = -1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                bound = mid;
+                if (isFirst) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return bound;
+    }
+};
