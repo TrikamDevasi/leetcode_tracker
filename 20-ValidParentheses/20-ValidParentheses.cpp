9428f1b49117bd79ff39f7aff8dff9a9b59a9efc
Trikam Devasi <trikam.devasi.cg@outlook.com>
2026-07-30 10:25:12 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.cpp
@@ -0,0 +1,15 @@
+// Last updated: 30/07/2026, 10:25:12
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c:s)
+        if(c=='(')st.push(')');
+        else if(c=='{') st.push('}');
+        else if(c=='[') st.push(']');
+        else if(st.empty()||st.top()!=c)return false;
+        else st.pop();
+        return st.empty();
+           
+    }
+};
