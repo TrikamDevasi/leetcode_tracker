77ee5d83ea0b13bd33f3988912957155372e3b73
Trikam Devasi <trikam.devasi.cg@outlook.com>
2026-07-30 10:21:53 +0530

    Create 2277-CountEqualAndDivisiblePairsInAnArray

---
+++ 2277-CountEqualAndDivisiblePairsInAnArray/2277-CountEqualAndDivisiblePairsInAnArray.cpp
@@ -0,0 +1,17 @@
+// Last updated: 30/07/2026, 10:21:53
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int k) {
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+           for(int j=i+1;j<nums.size();j++){
+            if(nums[i]==nums[j]){
+                if((i*j)%k==0){
+                    count++;
+                }
+            }
+           } 
+        }
+        return count;
+    }
+};
