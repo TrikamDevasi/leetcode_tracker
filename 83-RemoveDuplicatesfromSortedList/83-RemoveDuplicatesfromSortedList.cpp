3838e2492a9d5fbe68b265652b8fade6e3763314
Trikam Devasi <trikam.devasi.cg@outlook.com>
2026-08-07 13:47:51 +0530

    File updated at 07/08/2026, 13:47:51

--- 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.cpp
+++ 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.cpp
@@ -1,19 +1,29 @@
-// Last updated: 04/08/2026, 14:29:58
-1class Solution {
-2public:
-3    ListNode* deleteDuplicates(ListNode* head) {
-4        // Base case: if the list is empty, return nullptr immediately
-5        if (head == nullptr) return nullptr;
-6
-7        ListNode* curr = head;
-8        while (curr->next != nullptr) {
-9            if (curr->val == curr->next->val) {
-10                curr->next = curr->next->next;
-11            } else {
-12                curr = curr->next;
-13            }
-14        }
-15        
-16        return head;
-17    }
-18};
+// Last updated: 07/08/2026, 13:47:51
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* deleteDuplicates(ListNode* head) {
+14        ListNode* current = head;
+15
+16        while (current != nullptr && current->next != nullptr) {
+17            if (current->val == current->next->val) {
+18                ListNode* temp = current->next;
+19                current->next = current->next->next;
+20              
+21            } else {
+22                current = current->next;
+23            }
+24        }
+25
+26        return head;
+27    }
+28};
