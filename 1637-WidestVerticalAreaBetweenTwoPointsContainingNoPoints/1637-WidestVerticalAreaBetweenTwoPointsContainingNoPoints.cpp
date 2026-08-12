253cf8b41b42c70217300a83e69e4447ca9fa79b
Trikam Devasi <trikam.devasi.cg@outlook.com>
2026-08-12 14:27:02 +0530

    File updated at 12/08/2026, 14:27:01

--- 1637-WidestVerticalAreaBetweenTwoPointsContainingNoPoints/1637-WidestVerticalAreaBetweenTwoPointsContainingNoPoints.cpp
+++ 1637-WidestVerticalAreaBetweenTwoPointsContainingNoPoints/1637-WidestVerticalAreaBetweenTwoPointsContainingNoPoints.cpp
@@ -1,17 +1,21 @@
-// Last updated: 12/08/2026, 14:24:20
+// Last updated: 12/08/2026, 14:27:01
 1class Solution {
 2public:
-3    int maxWidthOfVerticalArea(vector<vector<int>>& points) {
-4        sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
-5            return a[0] < b[0];
-6        });
-7
-8        int maxWidth = 0;
-9
-10        for (size_t i = 1; i < points.size(); ++i) {
-11            maxWidth = max(maxWidth, points[i][0] - points[i - 1][0]);
-12        }
-13
-14        return maxWidth;
-15    }
-16};
+3    vector<string> findRepeatedDnaSequences(string s) {
+4        vector<string> result;
+5        if (s.length() < 10) return result;
+6
+7        unordered_map<string, int> count;
+8        for (int i = 0; i <= (int)s.length() - 10; ++i) {
+9            string sub = s.substr(i, 10);
+10            count[sub]++;
+11        }
+12        for (const auto& [sub, freq] : count) {
+13            if (freq > 1) {
+14                result.push_back(sub);
+15            }
+16        }
+17
+18        return result;
+19    }
+20};
